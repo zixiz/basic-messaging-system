a24b0c2ec9364937aa79bec8f6736ef26eac1fe3
zixiz <gshachargilad@gmail.com>
2020-06-25 12:49:44 +0300

    deploy

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/auth', authRouter);
 app.use('/messages',verifyToken ,messagesRouter);
 
-app.get("/*", function (req, res) {
+app.get("*", function (req, res) {
   res.sendFile(path.resolve(__dirname, './public', 'index.html'));
 })
 
